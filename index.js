e29a33d1144cb2effddac92585fb9d2a080b49e6
Soyeb <tssoyeb@gmail.com>
2022-06-25 22:25:16 +0600

    create reducer

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ const orderCake=()=>{
     }
 }
 
+const initialState ={
+    numOfCakes:10,
+    // anotherProperty:0,
+}
+
+//(previousState, action)=>newState
+const reducer =(state = initialState, action)=>{
+
+    switch(action.type){
+        case CAKE_ORDERED:
+            return{
+                ...state,
+                numOfCakes:state.numOfCakes - 1,
+            }
+            default:
+                return state;
+    }
+
+}
+
+const store = createStore(reducer)
+console.log('Initial State:',store.getState);
+
