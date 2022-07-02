1588e6cdb7dd7cd9ddca82a02de9f97812143f7a
Soyeb <tssoyeb@gmail.com>
2022-07-02 10:18:02 +0600

    nested state management with spread operator

---
+++ nested-state.js
@@ -0,0 +1,41 @@
+const redux=require('redux')
+const initialState = {
+    name: 'Soyeb Mahir',
+    address: {
+        street: 'Mosque road',
+        city: 'Dhaka',
+        state: 'Bangladesh'
+    }
+}
+
+const STREET_UPDATED = 'STREET_UPDATED'
+const updatedStreer = (street) => {
+    return {
+        type: STREET_UPDATED,
+        payload: street,
+    }
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case STREET_UPDATED:
+            return {
+                ...state,
+                address: {
+                    ...state.address,
+                    street: action.payload 
+                },
+            }
+            default:{
+                return state
+            }
+    }
+}
+
+const store = redux.createStore(reducer)
+console.log('Initial State',store.getState());
+const unsubscribe=store.subscribe(()=>{
+    console.log('Updated State', store.getState());
+})
+store.dispatch(updatedStreer('School Road'))
+unsubscribe();
